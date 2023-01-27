d2c7931df193eb301715e544e09fff0e3d1645c6
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-01-27 10:04:05 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,262 @@
-const addMessage = document.getElementById(btn);
-
-const addSum = (a, b) => {
-    return a + b
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
 };
 
-const arrUsers = ["John", "Andrew", "Mike", "Anne", "Julia"];
+function testEqual(val) {
+  if (val == 12) {
+    return "Equal";
+  }
+  return "Not Equal";
+};
+testEqual(10);
 
-function newUsers(users){
-    arrUsers.push("");
+function testStrict(val) {
+  if (val===7) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+testStrict(10);
+
+function compareEquality(a, b) {
+  if (a === b) { 
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+compareEquality(10, "10");
+
+function testNotEqual(val) {
+  if (val != 99) {
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testNotEqual(10);
+
+function testGreaterThan(val) {
+  if (val > 100) { 
+    return "Over 100";
+  }
+
+  if (val > 10) {  
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  
+    return "Smaller Than or Equal to 12";
+  }
+
+  if (val <= 24) {  
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+testLessOrEqual(10);
+
+function testLogicalAnd(val) {
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+  }
+    return "No";
+}
+
+testLogicalAnd(10);
+
+function testLogicalOr(val) {
+  
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+    return "Inside";
 };
 
-document.getElementById(input) = function(newUsers){
-    newUsers.bind();
-};  
+function testElse(val) {
+  let result = "";
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+    return result;
+};
 
-console.log(arrUsers);
+function orderMyLogic(val) {
+  if(val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+orderMyLogic(7);
+
+function abTest(a, b) {
+  if(a < 0 || b < 0){
+    return undefined;
+}
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+
+function checkObj(obj, checkProp) {
+  if (obj.hasOwnProperty(checkProp)) {
+    return obj[checkProp];
+  } else {
+    return "Not Found";
+  }
+};
+
+function test(val) {
+  if (val > 100) { 
+    return "Over 100";
+  }
+
+  if (val > 10) {  
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+test(10);
+
+function users(val) {
+  
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+    return "Inside";
+};
+
+
+function checkScope() {
+  "use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+};
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14
+  };
+
+Object.freeze(MATH_CONSTANTS);
+
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch(ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+const myConcat = (arr1, arr2) => {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+const sum = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+}
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+arr2 = [...arr1];  
+console.log(arr2);
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
+
+function removeFirstTwo(list) {
+  const [, , ...shorterList] = list;
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+const half = ({ max, min }) => (max + min) / 2.0;
+
+function updateRecords(records, id, prop, value) {
+  if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && value !== "" && records[id].hasOwnProperty("tracks") === false) {
+    records[id][prop] = [value];
+  } else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value);
+  } else if (value === "") {
+    delete records[id][prop];
+  }
+  return records;
+}
+
+const myArray = [];
+  for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+};
+
+function updateRecords(records, id, prop, value) {
+  if (prop !== "tracks" && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && value !== "" && records[id].hasOwnProperty("tracks") === false) {
+    records[id][prop] = [value];
+  } else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value);
+  } else if (value === "") {
+    delete records[id][prop];
+  }
+  return records;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
