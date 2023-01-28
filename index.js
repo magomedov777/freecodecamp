6688c1bc5f805b121ed78f99ffa7916ed4659ef9
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-01-28 09:07:07 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -249,15 +249,8 @@ function updateRecords(records, id, prop, value) {
 };
 
 
-function testElseIf(val) {
-  if (val > 10) {
-    return "Greater than 10";
-  } else if (val < 5) {
-    return "Smaller than 5";
-  } else {
-    return "Between 5 and 10";
-  }
-};
+
+
 
 
 
