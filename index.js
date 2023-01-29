88bcd0b57c7a5974db1d2f280ca4696b0f602722
Arkadiy <72151941+magomedov777@users.noreply.github.com>
2023-01-29 08:40:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -250,9 +250,14 @@ function testElseIf(val) {
   }
 };
 
+function abcTest(a, b) {
+  if(a < 0 || b < 0){
+    return undefined;
+}
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 
 
-
+  
 
 
 
